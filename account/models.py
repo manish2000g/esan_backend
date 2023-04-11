a2427f741103a4ee59054022b37e4e91f6368bde
manish2000g <gautammanish700@gmail.com>
2023-04-11 13:54:43 +0545

    account section

--- account/models.py
+++ account/models.py
@@ -1,13 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_player = models.BooleanField(default=True)
-    is_organization = models.BooleanField(default=False)
-    is_organizer = models.BooleanField(default=False)
-    is_blog_writer = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     is_player = models.BooleanField(default=True)
+#     is_organization = models.BooleanField(default=False)
+#     is_organizer = models.BooleanField(default=False)
+#     is_blog_writer = models.BooleanField(default=False)
+#     is_admin = models.BooleanField(default=False)
 
-    def __str__(self) -> str:
-        return self.user.first_name
+#     def __str__(self) -> str:
+#         return self.user.first_name
+    
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    profile_picture = models.ImageField(upload_to='media/static/images/player_profile_pictures/')
+    country = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class BlogWriter(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    bio = models.TextField(max_length=500)
+    profile_picture = models.ImageField(upload_to='media/static/images/blog_profile_pictures/')
+    website = models.URLField(max_length=200)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+class Organizer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='media/static/images/organizer_logos/')
+    description = models.TextField(max_length=500)
+    website = models.URLField(max_length=200)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+class Organization(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organization_name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='media/static/images/organization_logos/')
+    description = models.TextField(max_length=500)
+    website = models.URLField(max_length=200)
+    email = models.EmailField(max_length=254)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.organization_name
+    
--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import BlogWriter, Organization, Organizer, Player
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
@@ -23,28 +23,87 @@ class CustomAuthToken(ObtainAuthToken):
             }
         )
     
+# @api_view(['POST'])
+# def CreateUserProfile(request):
+#     first_name = request.POST['first_name']
+#     last_name = request.POST['last_name']
+#     email = request.POST['email']
+#     password = request.POST['password']
+#     is_player = request.POST.get('is_player',False) == True
+#     is_organization = request.POST.get('is_organization',False) == True
+#     is_organizer = request.POST.get('is_organizer',False) == True
+#     is_blog_writer = request.POST.get('is_blog_writer',False) == True
+#     is_admin = request.POST.get('is_admin', False) ==True
+
+#     # Create a new User object
+#     user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_name=last_name)
+
+#     # Create a new UserProfile object
+#     user_profile = UserProfile.objects.create(user=user, is_player=is_player, is_organization=is_organization, is_organizer=is_organizer, is_blog_writer=is_blog_writer)
+
+#     # Generate a new token for the user
+#     token, created = Token.objects.get_or_create(user=user)
+
+#     # Return a response with the token
+#     return Response({
+#         'token': token.key,
+#         'user_id': user.id,
+#         'email': user.email,
+#         'first_name': user.first_name,
+#         'last_name': user.last_name
+#     })
+
 @api_view(['POST'])
 def CreateUserProfile(request):
     first_name = request.POST['first_name']
     last_name = request.POST['last_name']
     email = request.POST['email']
     password = request.POST['password']
-    is_player = request.POST.get('is_player',False) == True
-    is_organization = request.POST.get('is_organization',False) == True
-    is_organizer = request.POST.get('is_organizer',False) == True
-    is_blog_writer = request.POST.get('is_blog_writer',False) == True
-    is_admin = request.POST.get('is_admin', False) ==True
+    is_player = request.POST.get('is_player', "False") == "True"
+    is_blog_writer = request.POST.get('is_blog_writer', "False") == "True"
+    is_organizer = request.POST.get('is_organizer', "False") == "True"
+    is_organization = request.POST.get('is_organization', "False") == "True"
 
-    # Create a new User object
     user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_name=last_name)
 
-    # Create a new UserProfile object
-    user_profile = UserProfile.objects.create(user=user, is_player=is_player, is_organization=is_organization, is_organizer=is_organizer, is_blog_writer=is_blog_writer)
+    if is_player:
+        name = request.POST['name']
+        country = request.POST['country']
+        phone_number = request.POST['phone_number']
+        profile_picture = request.FILES.get('profile_picture', None)
+        player = Player.objects.create(user=user, name=name, country=country, phone_number=phone_number, profile_picture=profile_picture)
+        player.save()
 
-    # Generate a new token for the user
+    elif is_blog_writer:
+        name = request.POST['name']
+        bio = request.POST['bio']
+        website = request.POST['website']
+        email = request.POST['email']
+        profile_picture = request.FILES.get('profile_picture', None)
+        blog_writer = BlogWriter.objects.create(user=user, name=name, bio=bio, website=website, email=email, profile_picture=profile_picture)
+        blog_writer.save()
+
+    elif is_organizer:
+        name = request.POST['name']
+        description = request.POST['description']
+        website = request.POST['website']
+        email = request.POST['email']
+        logo = request.FILES.get('logo', None)
+        organizer = Organizer.objects.create(user=user, name=name, description=description, website=website, email=email, logo=logo)
+        organizer.save()
+
+    elif is_organization:
+        organization_name = request.POST['organization_name']
+        description = request.POST['description']
+        website = request.POST['website']
+        email = request.POST['email']
+        address = request.POST['address']
+        logo = request.FILES.get('logo', None)
+        organization = Organization.objects.create(user=user, organization_name=organization_name, description=description, website=website, email=email, address=address, logo=logo)
+        organization.save()
+
     token, created = Token.objects.get_or_create(user=user)
 
-    # Return a response with the token
     return Response({
         'token': token.key,
         'user_id': user.id,
@@ -52,3 +111,10 @@ def CreateUserProfile(request):
         'first_name': user.first_name,
         'last_name': user.last_name
     })
+
+
+
+
+
+
+# organization many to many 
