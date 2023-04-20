941b57ab1f87b2393e041af8231e5dcff3f1a32d
vishaldhakal <vishaldhakal96@gmail.com>
2023-04-20 12:07:19 +0545

    used postgres

--- esan_backend/settings.py
+++ esan_backend/settings.py
@@ -69,10 +69,22 @@ WSGI_APPLICATION = 'esan_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "esan",
+        "USER": "vishal",
+        "PASSWORD": "DatabaseUserPassword",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
