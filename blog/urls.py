8373fdbc23211fd9f3a5d7024a50dba1d1d567f7
manish2000g <gautammanish700@gmail.com>
2023-04-30 14:52:40 +0545

    updated crud tag

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Article_Category_Detail, Create_Article_Category, Get_Article_Category, RetriveArticles, Article_Detail, FeaturedArticles, PopularArticles,CreateArticle, Update_Article_Category,UpdateArticle,DeleteArticle
+from .views import Article_Category_Detail, Create_Article_Category, Create_Tag, Delete_Tag, Get_Article_Category, Get_Tag, RetriveArticles, Article_Detail, FeaturedArticles, PopularArticles,CreateArticle, Update_Article_Category, Update_Tag,UpdateArticle,DeleteArticle
 
 urlpatterns = [
     path('create-article_category/', Create_Article_Category, name = 'create_article_category'),
@@ -11,6 +11,10 @@ urlpatterns = [
     path('update-article/<int:article_id>/', UpdateArticle, name = 'update_articles'),
     path('delete-article/<int:article_id>/', DeleteArticle, name = 'delete_articles'),
     path('articles/', RetriveArticles, name = 'get_articles'),
+    path('create-tag/', Create_Tag, name = 'create_tag'),
+    path('update-article/<int:tag_id>/', Update_Tag, name = 'update_tag'),
+    path('delete-article/<int:tag_id>/', Delete_Tag, name = 'delete_tags'),
+    path('tags/', Get_Tag, name = 'get_tags'),
     path('articles/<str:slug>/', Article_Detail, name = 'article_details'),
     path('featured_articles/', FeaturedArticles, name = 'featured_articles'),
     path('popular_articles/', PopularArticles, name = 'popular_articles')
