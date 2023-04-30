7ed1f771ef5ec5d3f01bf62694592075345e8fdd
manish2000g <gautammanish700@gmail.com>
2023-04-30 15:08:57 +0545

    updated urls account

--- account/urls.py
+++ account/urls.py
@@ -9,9 +9,9 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
      path("create-user-profile/",views.CreateUserProfile, name="create_user_profile"),
      path("get-user-profile/",views.GetUserProfile, name="get_user_profile"),
     #  path('teams/', views.team_list),
