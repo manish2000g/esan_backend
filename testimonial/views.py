0d007100f601bae8a881d948bae77b5e0069faf3
vishaldhakal <vishaldhakal96@gmail.com>
2023-05-13 16:47:04 +0545

    DAFv'

--- testimonial/views.py
+++ testimonial/views.py
@@ -1,61 +1,52 @@
 
-# Create your views here.
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Testimonial
 from .serializers import TestimonialSerializer
+from account.models import UserProfile
+
+@api_view(['GET'])
+def testimonials(request):
+    testimonial = Testimonial.objects.filter(is_verified=True)
+    testimonial_ser = TestimonialSerializer(testimonial,many=True)
+    return Response({"testimonials": testimonial_ser.data}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def submit_testimonial(request):
-    user = request.POST['user']
-    description = request.POST['description']
-    rating =request.POST['rating']
-
-    testimonial = Testimonial(
-        user = user,
-        description = description,
-        rating = rating
-    )
+    user = request.user
+    description = request.POST.get("description")
+    rating = request.POST.get("rating")
+    testimonial = Testimonial(user=user,description=description,rating=rating)
     testimonial.save()
-    return Response({"Testimonial created successfully"})
+    return Response({"success": "Submitted Sucessfully"}, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def verify_testimonial(request):
+    username = request.GET.get("name")
+    user = UserProfile.objects.get(username=username)
+    if user.role == "Admin":
+        testimonial = Testimonial.objects.get(user=user)
+        testimonial.is_verified = True
+        testimonial.save()
+        return Response({"success": "Submitted Sucessfully"}, status=status.HTTP_202_ACCEPTED)
+    else:
+        return Response({"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def update_testimonial(request):
+    username = request.GET.get("name")
+    user = UserProfile.objects.get(username=username)
+    testimonial = Testimonial.objects.get(user=user)
+    description = request.POST.get("description")
+    rating = request.POST.get("rating")
     
-@permission_classes([IsAuthenticated])
-@api_view(['GET'])
-def get_testimonials(request):
-    testimonials = Testimonial.objects.all()
-    serializer = TestimonialSerializer(testimonials, many=True)
-    return Response(serializer.data)
-
-@permission_classes([IsAuthenticated])
-@api_view(['GET'])
-def get_testimonial(request, id):
-    testimonial = Testimonial.objects.get(id=id)
-    serializer = TestimonialSerializer(testimonial)
-    return Response(serializer.data)
-
-@permission_classes([IsAuthenticated])
-@api_view(['PUT'])
-def edit_testimonial(request, id):
-    testimonial = Testimonial.objects.get(id=id)
-    user = request.POST.get('user')
-    description = request.POST.get('description')
-    rating =request.POST.get('rating')
-
-    testimonial.user = user
     testimonial.description = description
     testimonial.rating = rating
 
     testimonial.save()
-    return Response({"Testimonial Updated Successfully"})
-    
-
-@permission_classes([IsAuthenticated])
-@api_view(['DELETE'])
-def delete_testimonial(request, pk):
-    testimonial = Testimonial.objects.get(pk=pk)
-    testimonial.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
- 
+    return Response({"success": "Updated Sucessfully"}, status=status.HTTP_200_OK)
