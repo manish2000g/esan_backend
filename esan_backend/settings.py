02bff61dc86483275ac6fdc9648f3412eb9e3351
vishaldhakal <vishaldhakal96@gmail.com>
2023-05-13 13:09:45 +0545

    Dfv

--- esan_backend/settings.py
+++ esan_backend/settings.py
@@ -74,15 +74,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'esan_backend.wsgi.application'
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} 
+}  """
 
 
-""" DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "esan",
@@ -91,7 +91,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "",
     }
-} """
+}
 
 
 # Password validation
