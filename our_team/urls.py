0ed7a4bd6ccd2c2dd0ece5994294b574dd032b99
vishaldhakal <vishaldhakal96@gmail.com>
2023-05-23 14:17:45 +0545

    dsfV

--- our_team/urls.py
+++ our_team/urls.py
@@ -3,7 +3,7 @@ from our_team.views import OurTeams,CreateOurTeam,UpdateOurTeam,DeleteOurTeam
 
 urlpatterns = [
     path('ourteam-list/', OurTeams, name = 'ourteam_list'),
-    path('creat-ourteam/', CreateOurTeam, name = 'create_our_team'),
+    path('create-ourteam/', CreateOurTeam, name = 'create_our_team'),
     path('update-ourteam/', UpdateOurTeam, name = 'update_ourteam'),
     path('delete-ourteam/', DeleteOurTeam, name = 'delete_ourteam'),
 ]
